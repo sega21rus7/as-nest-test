526d80f3632ae52147cbba11afe8c2488f5641a0
sega21rus7 <sega21rus98@mail.ru>
2021-05-14 18:07:24 +0300

    project name changed

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default () => ({
   mongodb_host: 'localhost',
   mongodb_port: 27017,
-  mongodb_dbName: 'as-nest-test',
+  mongodb_dbName: 'as-products',
   mongodb_username: '',
   mongodb_password: '',
 });
